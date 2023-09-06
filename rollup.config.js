6c4d6320563ebbf938770341eb15b78813a40daf
AI Tech <143031997+AI-Tech2023@users.noreply.github.com>
2023-09-06 09:20:25 +0200

    Update rollup.config.js
    Reset to Original

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ import { uglify } from "rollup-plugin-uglify";
 //import serve from "rollup-plugin-serve";
 //import livereload from "rollup-plugin-livereload";
 
-const extensions = [".css", ".ts", ".tsx"]; //added css files for UI changes
+const extensions = [".ts", ".tsx"];
 
 const indexConfig = {
   plugins: [
