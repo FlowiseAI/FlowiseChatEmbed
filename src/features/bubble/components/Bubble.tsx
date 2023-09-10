06a535237054afe497224386dac0c224ceed05b3
AI Tech <143031997+AI-Tech2023@users.noreply.github.com>
2023-09-10 14:52:44 +0200

    Build

--- src/features/bubble/components/Bubble.tsx
+++ src/features/bubble/components/Bubble.tsx
@@ -43,7 +43,7 @@ export const Bubble = (props: BubbleProps) => {
                 class={
                     `fixed sm:right-5 rounded-lg w-full sm:w-[400px] max-h-[704px]` +
                     (isBotOpened() ? ' opacity-1' : ' opacity-0 pointer-events-none') +
-                    (props.theme?.button?.size === 'large' ? ' bottom-24' : ' bottom-20')
+                    (props.theme?.button?.size === 'large' ? ' bottom-48' : ' bottom-20') // large: bottom from 24 to 48
                 }
             >
                 <Show when={isBotStarted()}>
