0419e273e1d6eb878516c0aaa87003fe924033b9
kevthedev100 <136115811+kevthedev100@users.noreply.github.com>
2023-09-14 22:16:44 +0200

    weitere änderung

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,14 +54,14 @@ export const Badge = (props: Props) => {
     }}>Hier zu unserer Webseite:
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://videyou.de'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Videyou</span>
+        <span>Videyou</span>
       </a>
     </span>
   )
