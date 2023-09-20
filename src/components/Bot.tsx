fb255602093ab466210d4bd4e3b9d236529c8770
DrShea123 <136972291+DrShea123@users.noreply.github.com>
2023-09-20 16:09:28 +0100

    No Branding

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -391,7 +391,6 @@ export const Bot = (props: BotProps & { class?: string }) => {
                         onSubmit={handleSubmit}
                     />
                 </div>
-                <Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} />
                 <BottomSpacer ref={bottomSpacer} />
             </div>
             {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
