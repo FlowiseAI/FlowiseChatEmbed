7a9e9350f4c781242af915d6f9cead7b2e871664
AI Tech <143031997+AI-Tech2023@users.noreply.github.com>
2023-09-25 22:37:05 +0200

    Changed Hyperlink and Badge
    Change Badge to AI Tech and make hyperlinks open in a new tab with "'noopener,noreferrer'"

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Andy</span>
+        <span> AI Tech</span>
       </a>
     </span>
   )
