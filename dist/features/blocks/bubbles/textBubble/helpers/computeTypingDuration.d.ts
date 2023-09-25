8db808dc1c784de64f1e6ef3f1c3db931ac75ead
Henry <hzj94@hotmail.com>
2023-09-25 12:23:40 +0100

    cleanup and update to 1.0.13

--- dist/features/blocks/bubbles/textBubble/helpers/computeTypingDuration.d.ts
+++
@@ -1,3 +0,0 @@
-import type { TypingEmulation } from '@/schemas';
-export declare const computeTypingDuration: (bubbleContent: string, typingSettings: TypingEmulation) => number;
-//# sourceMappingURL=computeTypingDuration.d.ts.map
