d59660331291db8ae5becd9e8d6b26233665c659
premiumleads <87935840+premiumleads@users.noreply.github.com>
2023-09-26 10:29:18 +0200

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,17 +51,17 @@ export const Badge = (props: Props) => {
       "text-align": 'center',
       color: props.poweredByTextColor ?? defaultTextColor,
       "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by
+    }}> Assistant
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={''}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Virtuel </span>
       </a>
     </span>
   )
