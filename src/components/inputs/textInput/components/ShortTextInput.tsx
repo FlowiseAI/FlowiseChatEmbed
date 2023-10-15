97a83db4afc5a67f755392273ff1d510aff766e4
AI Tech <143031997+AI-Tech2023@users.noreply.github.com>
2023-10-15 14:11:54 +0200

    Update ShortTextInput.tsx

--- src/components/inputs/textInput/components/ShortTextInput.tsx
+++ src/components/inputs/textInput/components/ShortTextInput.tsx
@@ -1,27 +1,3 @@
-/* import { splitProps } from 'solid-js'
-import { JSX } from 'solid-js/jsx-runtime'
-
-type ShortTextInputProps = {
-    ref: HTMLInputElement | undefined
-    onInput: (value: string) => void
-    fontSize?: number
-} & Omit<JSX.InputHTMLAttributes<HTMLInputElement>, 'onInput'>
-
-export const ShortTextInput = (props: ShortTextInputProps) => {
-    const [local, others] = splitProps(props, ['ref', 'onInput'])
-
-    return (
-        <input
-            ref={props.ref}
-            class='focus:outline-none bg-transparent px-4 py-4 flex-1 w-full text-input'
-            type='text'
-            style={{ 'font-size': props.fontSize ? `${props.fontSize}px` : '16px' }}
-            onInput={(e) => local.onInput(e.currentTarget.value)}
-            {...others}
-        />
-    )
-}
-*/
 import { splitProps } from 'solid-js'
 import { JSX } from 'solid-js/jsx-runtime'
 
