6bd1f249b7ef6f4d74f5fcf4081e0cf547cf846f
andrew-downie-realty-ai <andrew.downie@realty-ai.ca>
2023-10-15 22:18:40 -0400

    fixed text for realty ai

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,7 +51,7 @@ export const Badge = (props: Props) => {
       "text-align": 'center',
       color: props.poweredByTextColor ?? defaultTextColor,
       "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by
+    }}>Powered by 
       <a
         ref={liteBadge}
         href={'https://www.realty-ai.ca'}
