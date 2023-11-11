158178745df7b5c84cfe445a9e7caa914baac2e5
AI Tech <143031997+AI-Tech2023@users.noreply.github.com>
2023-11-11 13:35:15 +0100

    Squash Merge Badge Branch to Main Branch (#2)
    * Change Badge from FW to AI Tech
    
    * Build

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,14 +54,14 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://math-angel.co.uk'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> AI Tech</span>
       </a>
     </span>
   )
