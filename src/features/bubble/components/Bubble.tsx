a9817829feb047e274cb4555702c2d26bcfeff9d
AI Tech <143031997+AI-Tech2023@users.noreply.github.com>
2023-11-11 13:55:05 +0100

    Squash and Merge Feature/default open to main (#3)
    * Change default behavior of chat bubble from closed to open
    
    * Build

--- src/features/bubble/components/Bubble.tsx
+++ src/features/bubble/components/Bubble.tsx
@@ -12,8 +12,8 @@ export type BubbleProps = BotProps & BubbleParams
 export const Bubble = (props: BubbleProps) => {
     const [bubbleProps] = splitProps(props, ['theme'])
 
-    const [isBotOpened, setIsBotOpened] = createSignal(false)
-    const [isBotStarted, setIsBotStarted] = createSignal(false)
+    const [isBotOpened, setIsBotOpened] = createSignal(true) // Changed by AIT from false to true. So that the chatbot is always opened by default
+    const [isBotStarted, setIsBotStarted] = createSignal(true) // Changed by AIT from false to true. So that the chatbot is always opened by default
 
     const openBot = () => {
         if (!isBotStarted()) setIsBotStarted(true)
