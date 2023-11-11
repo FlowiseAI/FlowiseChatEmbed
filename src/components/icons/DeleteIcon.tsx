2ec07232a864d855c509c44fffc96831560fa988
AI Tech <143031997+AI-Tech2023@users.noreply.github.com>
2023-11-11 15:04:33 +0100

    Change Delete Icon in Chat Bubble (#5)

--- src/components/icons/DeleteIcon.tsx
+++ src/components/icons/DeleteIcon.tsx
@@ -1,9 +1,12 @@
 import { JSX } from 'solid-js/jsx-runtime'
 const defaultButtonColor = '#3B81F6'
 export const DeleteIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
-	<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-refresh" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke={props.color ?? defaultButtonColor} fill="none" stroke-linecap="round" stroke-linejoin="round">
-		<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-		<path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4"></path>
-		<path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"></path>
-	</svg>
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke={props.color ?? defaultButtonColor} fill="none" stroke-linecap="round" stroke-linejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <path d="M4 7l16 0"></path>
+   <path d="M10 11l0 6"></path>
+   <path d="M14 11l0 6"></path>
+   <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+   <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+</svg>
 )
