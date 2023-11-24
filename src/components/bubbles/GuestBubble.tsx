3a4a7b8819233eabdb60dede42acb003056f409e
hudsonargollo <105505660+hudsonargollo@users.noreply.github.com>
2023-11-24 19:04:37 -0300

    Update GuestBubble.tsx

--- src/components/bubbles/GuestBubble.tsx
+++ src/components/bubbles/GuestBubble.tsx
@@ -10,7 +10,7 @@ type Props = {
   textColor?: string
 }
 
-const defaultBackgroundColor = '#3B81F6'
+const defaultBackgroundColor = '#962922'
 const defaultTextColor = '#ffffff'
 
 Marked.setOptions({ isNoP: true })
