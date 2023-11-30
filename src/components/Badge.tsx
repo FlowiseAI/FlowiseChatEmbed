3a785525731e59caaed652a9ee1eb4b06e5fd9dc
gokuljs <gokuljs029@gmail.com>
2023-11-30 22:54:25 +0530

    presttier setup

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,57 +1,56 @@
-import { onCleanup, onMount } from 'solid-js'
+import { onCleanup, onMount } from 'solid-js';
 
 type Props = {
-  botContainer: HTMLDivElement | undefined
-  poweredByTextColor?: string
-  badgeBackgroundColor?: string
-}
+  botContainer: HTMLDivElement | undefined;
+  poweredByTextColor?: string;
+  badgeBackgroundColor?: string;
+};
 
-const defaultTextColor = '#303235'
+const defaultTextColor = '#303235';
 
 export const Badge = (props: Props) => {
-  let liteBadge: HTMLAnchorElement | undefined
-  let observer: MutationObserver | undefined
+  let liteBadge: HTMLAnchorElement | undefined;
+  let observer: MutationObserver | undefined;
 
   const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
     mutations.forEach((mutation) => {
       mutation.removedNodes.forEach((removedNode) => {
-        if (
-          'id' in removedNode &&
-          liteBadge &&
-          removedNode.id == 'lite-badge'
-        ) {
-          console.log("Sorry, you can't remove the brand 😅")
-          props.botContainer?.append(liteBadge)
+        if ('id' in removedNode && liteBadge && removedNode.id == 'lite-badge') {
+          console.log("Sorry, you can't remove the brand 😅");
+          props.botContainer?.append(liteBadge);
         }
-      })
-    })
-  }
+      });
+    });
+  };
 
   onMount(() => {
-    if (!document || !props.botContainer) return
-    observer = new MutationObserver(appendBadgeIfNecessary)
+    if (!document || !props.botContainer) return;
+    observer = new MutationObserver(appendBadgeIfNecessary);
     observer.observe(props.botContainer, {
       subtree: false,
       childList: true,
-    })
-  })
+    });
+  });
 
   onCleanup(() => {
-    if (observer) observer.disconnect()
-  })
+    if (observer) observer.disconnect();
+  });
 
   return (
-    <span style={{
-      "font-size": '13px',
-      position: 'absolute',
-      bottom: 0,
-      padding: '10px',
-      margin: 'auto',
-      width: '100%',
-      "text-align": 'center',
-      color: props.poweredByTextColor ?? defaultTextColor,
-      "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by
+    <span
+      style={{
+        'font-size': '13px',
+        position: 'absolute',
+        bottom: 0,
+        padding: '10px',
+        margin: 'auto',
+        width: '100%',
+        'text-align': 'center',
+        color: props.poweredByTextColor ?? defaultTextColor,
+        'background-color': props.badgeBackgroundColor ?? '#ffffff',
+      }}
+    >
+      Powered by
       <a
         ref={liteBadge}
         href={'https://flowiseai.com'}
@@ -59,10 +58,10 @@ export const Badge = (props: Props) => {
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
-        style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
+        style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
         <span> Flowise</span>
       </a>
     </span>
-  )
-}
+  );
+};
