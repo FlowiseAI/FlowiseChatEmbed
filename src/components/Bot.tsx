8c2f45b3373387dbe5a7751684495f71af2fc199
TonyMugwump <124447265+TonyMugwump@users.noreply.github.com>
2023-12-18 00:57:12 +0800

    Update Bot.tsx

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -521,7 +521,6 @@ export const Bot = (props: BotProps & { class?: string }) => {
             </div>
           </Show>
         </Show>
-        <Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} />
         <BottomSpacer ref={bottomSpacer} />
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
