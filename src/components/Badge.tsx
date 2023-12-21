ce0f9171511e1924c973a9d731ab0747acd2d207
prodij <james.han.la@gmail.com>
2023-12-20 19:48:46 -0800

    edit footer

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> AutoMinds</span> | <div>Privacy policy</div>
+        <span> AutoMinds</span> | <span>Privacy policy</span>
       </a>
     </span>
   );
