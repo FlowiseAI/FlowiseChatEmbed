bee19d0ef1b4cfab48f21130c1c85c76312bc5fe
LF <93954554+LeadsFarmer@users.noreply.github.com>
2024-01-01 17:54:37 +0000

    modified:   src/components/Badge.tsx 	modified:   src/components/Bot.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://leads-farmer.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Leads Farmer</span>
       </a>
     </span>
   );
