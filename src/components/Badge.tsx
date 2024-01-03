42e18ef7cf301b3c17d1204f9ddcf5e127132fc4
Keith Waldorf <keith@trelles.ai>
2024-01-02 17:15:10 -0800

    Committing changes again to the repo

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Work Smarter</span>
+        <span> Smarter</span>
       </a>
     </span>
   );
