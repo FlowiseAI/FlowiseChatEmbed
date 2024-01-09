f275561baab1f541ffa249146ae6461c54a36f1e
Ilango <rajagopalilango@gmail.com>
2024-01-09 19:42:17 +0530

    New build

---
+++ dist/components/inputs/textInput/components/TextInput.d.ts
@@ -0,0 +1,13 @@
+type Props = {
+    placeholder?: string;
+    backgroundColor?: string;
+    textColor?: string;
+    sendButtonColor?: string;
+    defaultValue?: string;
+    fontSize?: number;
+    disabled?: boolean;
+    onSubmit: (value: string) => void;
+};
+export declare const TextInput: (props: Props) => import("solid-js").JSX.Element;
+export {};
+//# sourceMappingURL=TextInput.d.ts.map
