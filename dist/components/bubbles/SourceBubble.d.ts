23807bd352b4a4222d7e204b4c015bf23638c20c
Ilango <rajagopalilango@gmail.com>
2024-01-09 19:43:57 +0530

    Delete build

--- dist/components/bubbles/SourceBubble.d.ts
+++
@@ -1,8 +0,0 @@
-type Props = {
-    pageContent: string;
-    metadata: object;
-    onSourceClick?: () => void;
-};
-export declare const SourceBubble: (props: Props) => import("solid-js").JSX.Element;
-export {};
-//# sourceMappingURL=SourceBubble.d.ts.map
--- dist/features/bubble/components/BubbleButton.d.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonTheme } from '../types';
-type Props = ButtonTheme & {
-    isBotOpened: boolean;
-    toggleBot: () => void;
-};
-export declare const BubbleButton: (props: Props) => import("solid-js").JSX.Element;
-export {};
-//# sourceMappingURL=BubbleButton.d.ts.map
