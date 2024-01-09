3b00b9a71a5254d0c444d8641ce59e715ce5e39a
Ilango <rajagopalilango@gmail.com>
2024-01-09 19:43:57 +0530

    Delete build

--- dist/components/bubbles/StarterPromptBubble.d.ts
+++
@@ -1,7 +0,0 @@
-type Props = {
-    prompt: string;
-    onPromptClick?: () => void;
-};
-export declare const StarterPromptBubble: (props: Props) => import("solid-js").JSX.Element;
-export {};
-//# sourceMappingURL=StarterPromptBubble.d.ts.map
--- dist/features/bubble/components/Bubble.d.ts
+++
@@ -1,5 +0,0 @@
-import { BubbleParams } from '../types';
-import { BotProps } from '../../../components/Bot';
-export type BubbleProps = BotProps & BubbleParams;
-export declare const Bubble: (props: BubbleProps) => import("solid-js").JSX.Element;
-//# sourceMappingURL=Bubble.d.ts.map
