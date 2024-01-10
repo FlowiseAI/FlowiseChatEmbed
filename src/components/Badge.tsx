7583adda4ec62a468e55c099ab5ab8193e773fa0
LF <93954554+LeadsFarmer@users.noreply.github.com>
2024-01-10 15:45:35 +0000

    remove brand

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,7 +50,7 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Fourni par
+      Powered by
       <a
         ref={liteBadge}
         href={'https://leads-farmer.com'}
