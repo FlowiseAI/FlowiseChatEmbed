51c7be1f3258346d106aecff643bbd55cc3384a8
Andrea Vitali <andrea.vitov@gmail.com>
2024-01-16 10:12:08 +0100

    restore to 0.0.8

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -195,7 +195,7 @@ export const Bot = (props: BotProps & { class?: string }) => {
     const rgx = new RegExp(`<pr sku=${product.sku}></pr>`);
     return msg.replace(
       rgx,
-      `<div class="max-w-xs m-4"><img src="${product.imageUrl}" alt="${product.name}" class="w-full aspect-square object-contain p-4 bg-white" /><a href="${product.affiliateLink}" class="w-full text-center border-x border-b border-black py-2 px-4 block bg-white text-black hover:bg-black hover:text-white no-underline hover:no-underline">ACQUISTA PRODOTTO</a></div>`,
+      `<div class="max-w-xs m-4"><img src="${product.imageUrl}" alt="${product.name}" class="w-full aspect-square object-contain p-4 bg-white" /><a target="blank" href="${product.affiliateLink}" class="w-full text-center border-x border-b border-black py-2 px-4 block bg-white text-black hover:bg-black hover:text-white no-underline hover:no-underline" style="font-family:Jost;font-size:11px;font-weight: 800;">ACQUISTA PRODOTTO</a></div>`,
     );
   }
   const updateLastMessageWithProduct = (product: Product) => {
@@ -581,6 +581,7 @@ export const Bot = (props: BotProps & { class?: string }) => {
             </div>
           </Show>
         </Show>
+        <BottomSpacer ref={bottomSpacer} />
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
     </>
