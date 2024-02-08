3a5940748b2c0db466075e1060cf6e64be67cb7b
creativevalley <158861856+creativevalley@users.noreply.github.com>
2024-02-08 05:00:16 +0300

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -60,7 +60,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> creative valley</span>
       </a>
     </span>
   );
