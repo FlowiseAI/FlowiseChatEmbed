ec2183ae0aad58e33aca22b3e12946ded8137fc0
creativevalley <158861856+creativevalley@users.noreply.github.com>
2024-02-17 14:43:17 +0000

    hiiiiiiiiiiiiii

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'creaitvevalley.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> creative valley</span>
       </a>
     </span>
   );
