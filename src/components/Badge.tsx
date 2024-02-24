84b19573753c1cea07c96b555c3b308a4a8893a2
Gatien <gatiendevictor@gmail.com>
2024-02-24 20:02:08 +0100

    initial

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://www.gatiendevictor.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> GD AI</span>
       </a>
     </span>
   );
