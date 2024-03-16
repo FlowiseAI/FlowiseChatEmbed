553d62c890a5f47f9fe08b6492f821bc6d4a3c1a
VSM97 <39551654+VSM97@users.noreply.github.com>
2024-03-16 08:12:35 +0530

    Update TypingBubble.tsx

--- src/components/TypingBubble.tsx
+++ src/components/TypingBubble.tsx
@@ -1,5 +1,5 @@
 export const TypingBubble = () => (
-  <div class="flex items-center">
+  <div class="flex items-center bg-transparent">
     <div class="w-2 h-2 mr-1 rounded-full bubble1" />
     <div class="w-2 h-2 mr-1 rounded-full bubble2" />
     <div class="w-2 h-2 rounded-full bubble3" />
