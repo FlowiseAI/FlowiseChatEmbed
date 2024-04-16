c488d493142e1ae25b4fefdfc1e837144e7d8f74
huseyintorun <85924424+huseyintorun@users.noreply.github.com>
2024-04-16 23:47:10 +0200

    Update Badge.tsx
    Last Update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> hercules-solutions</span>
+        <span> hercules-solutions </span>
       </a>
     </span>
   );
