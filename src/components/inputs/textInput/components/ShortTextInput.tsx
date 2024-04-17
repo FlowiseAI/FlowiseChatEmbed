6f246579c40aceb75aa72c53e427a59ae11fe1b7
Ilango <rajagopalilango@gmail.com>
2024-04-17 14:28:47 +0530

    Fix font family and textarea height when the input string is long

--- src/components/inputs/textInput/components/ShortTextInput.tsx
+++ src/components/inputs/textInput/components/ShortTextInput.tsx
@@ -1,4 +1,4 @@
-import { splitProps } from 'solid-js';
+import { createSignal, splitProps } from 'solid-js';
 import { JSX } from 'solid-js/jsx-runtime';
 
 type ShortTextInputProps = {
@@ -10,15 +10,29 @@ type ShortTextInputProps = {
 
 export const ShortTextInput = (props: ShortTextInputProps) => {
   const [local, others] = splitProps(props, ['ref', 'onInput']);
+  const [height, setHeight] = createSignal(56);
+
+  // @ts-expect-error: unknown type
+  const handleInput = (e) => {
+    if (props.ref) {
+      if (e.currentTarget.value === '') {
+        // reset height when value is empty
+        setHeight(56);
+      } else {
+        setHeight(e.currentTarget.scrollHeight - 24);
+      }
+      e.currentTarget.scrollTo(0, e.currentTarget.scrollHeight);
+      local.onInput(e.currentTarget.value);
+    }
+  };
 
   return (
     <textarea
       ref={props.ref}
-      class="focus:outline-none bg-transparent px-2 py-4 flex-1 w-full text-input disabled:opacity-50 disabled:cursor-not-allowed disabled:brightness-100"
+      class="focus:outline-none bg-transparent px-2 py-4 flex-1 w-full h-full min-h-[56px] max-h-[128px] text-input disabled:opacity-50 disabled:cursor-not-allowed disabled:brightness-100 "
       disabled={props.disabled}
-      style={{ 'font-size': props.fontSize ? `${props.fontSize}px` : '16px', resize: 'none' }}
-      onInput={(e) => local.onInput(e.currentTarget.value)}
-      rows={1}
+      style={{ 'font-size': props.fontSize ? `${props.fontSize}px` : '16px', resize: 'none', height: `${height()}px` }}
+      onInput={handleInput}
       {...others}
     />
   );
