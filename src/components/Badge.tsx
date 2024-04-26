b36fb11de606cb939c3ab02f12ae63e59f05ffa4
lukasTinnes <86609833+LukasTinnesSajo@users.noreply.github.com>
2024-04-26 14:51:58 +0200

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://sajoconsulting.de'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span>Sajo Consulting GmbH</span>
       </a>
     </span>
   );
