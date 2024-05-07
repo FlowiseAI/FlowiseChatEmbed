d545553c0834009ea4c04e0651afe5144149c9f5
niztal <nitzan.tal2@gmail.com>
2024-05-07 13:49:11 +0300

    powered by ✨ + link

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://apps.shopify.com/kesem-ai-product-recommender'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> ✨</span>
       </a>
     </span>
   );
