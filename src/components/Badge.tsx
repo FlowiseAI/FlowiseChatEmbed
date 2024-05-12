25ed680715a97c9fcea34d3abfdde506ee059c5a
sanket <sanket.terdal@gmail.com>
2024-05-12 12:28:18 +0530

    Updated badge file

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise Test</span>
+        <span> Flowise Test123</span>
       </a>
     </span>
   );
