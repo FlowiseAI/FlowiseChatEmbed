76eea247adaa698809c59255e0b0918eec81c52f
Aman Soni <aman.s@amazatic.com>
2024-05-22 17:21:58 +0530

    Icon size changed

---
+++ dist/components/icons/FeedbackIcon.d.ts
@@ -0,0 +1,3 @@
+import { JSX } from 'solid-js/jsx-runtime';
+export declare const FeedbackIcon: (props: JSX.SvgSVGAttributes<SVGSVGElement>) => JSX.Element;
+//# sourceMappingURL=FeedbackIcon.d.ts.map
--- src/components/icons/ClipboardIcon.tsx
+++ src/components/icons/ClipboardIcon.tsx
@@ -3,9 +3,9 @@ const defaultButtonColor = '#3B81F6';
 export const ClipboardIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon icon-tabler icon-tabler-refresh w-4 h-4"
-    width="24"
-    height="24"
+    class="icon icon-tabler icon-tabler-refresh w-5 h-5"
+    width="30"
+    height="30"
     viewBox="0 0 24 24"
     fill="none"
     stroke={props.color ?? defaultButtonColor}
