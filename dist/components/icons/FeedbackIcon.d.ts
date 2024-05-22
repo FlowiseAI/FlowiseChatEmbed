357a0d7d5f1c1a4697cdb8bad4cfd7f2334c774f
Aman Soni <aman.s@amazatic.com>
2024-05-22 17:27:56 +0530

    icon size changed

--- dist/components/icons/FeedbackIcon.d.ts
+++
@@ -1,3 +0,0 @@
-import { JSX } from 'solid-js/jsx-runtime';
-export declare const FeedbackIcon: (props: JSX.SvgSVGAttributes<SVGSVGElement>) => JSX.Element;
-//# sourceMappingURL=FeedbackIcon.d.ts.map
--- src/components/icons/ThumbsUpIcon.tsx
+++ src/components/icons/ThumbsUpIcon.tsx
@@ -3,9 +3,9 @@ const defaultButtonColor = '#3B81F6';
 export const ThumbsUpIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon icon-tabler icon-tabler-refresh w-4 h-4"
-    width="24"
-    height="24"
+    class="icon icon-tabler icon-tabler-refresh w-6 h-6"
+    width="36"
+    height="36"
     viewBox="0 0 24 24"
     fill="none"
     stroke={props.color ?? defaultButtonColor}
