abc93bd4527183b4919a270e825b330195e46647
SUP3RNOVA3 <142734293+SUP3RNOVA3@users.noreply.github.com>
2024-05-26 14:53:33 -0400

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span>HYP3RNOVA</span>
+        <span>AntiMateria</span>
       </a>
     </span>
   );
