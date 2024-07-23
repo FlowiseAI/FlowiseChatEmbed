1f7cd551f2f69d7624c3110037efbd2b63b36f64
andrew-downie-realty-ai <andrew.downie@realty-ai.ca>
2024-07-22 20:02:16 -0400

    made the bubble bigger once more

--- src/components/avatars/DefaultAvatar.tsx
+++ src/components/avatars/DefaultAvatar.tsx
@@ -7,7 +7,7 @@ export const DefaultAvatar = () => {
       <figure
         class={
           'flex justify-center items-center rounded-full text-white relative flex-shrink-0 ' +
-          (isMobileCheck() ? 'w-6 h-6 text-sm' : 'w-10 h-10 text-xl')
+          (isMobileCheck() ? 'w-8 h-8 text-sm' : 'w-10 h-10 text-xl')
         }
       >
         <img
@@ -16,7 +16,7 @@ export const DefaultAvatar = () => {
           class="rounded-full object-cover w-full h-full"
         />
             <svg viewBox="0 0 24 24" 
-              class = {isMobileCheck() ? 'w-2 h-2' : "w-4 h-4"}
+              class = {isMobileCheck() ? 'w-3 h-3' : "w-4 h-4"}
               style="position: absolute;top: 84%;left: 84%;transform: translate(-50%, -50%);">
                 <path fill="#3BF653" d="m2 12a10 10 0 1 1 10 10 10 10 0 0 1 -10-10z"/>
             </svg>
--- src/window.ts
+++ src/window.ts
@@ -21,7 +21,7 @@ type BotProps = {
     badgeText?:string,
 }
 
-const version = "realty-ai-bot-version:1.4"
+const version = "realty-ai-bot-version:1.5"
 export const initFull = (props: BotProps & { id?: string }) => {
     console.log(version)
     const data = sendRequest<any>({
