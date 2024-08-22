b64782b119d5a122ac2352606e8cbcc6d010c94e
Shane Muirhead <shane.muirhead@tribusdigital.com>
2024-08-22 17:37:53 +0100

    Added max-wdith to start bubble prompts

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -1,3 +1,5 @@
+import { wrap } from 'lodash';
+
 type Props = {
   prompt: string;
   onPromptClick?: () => void;
@@ -10,6 +12,11 @@ export const StarterPromptBubble = (props: Props) => (
       data-modal-toggle="defaultModal"
       class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
       onClick={() => props.onPromptClick?.()}
+      style={{
+        'max-width': '100%',
+        'word-wrap': 'break-word',
+         overflow: 'hidden',
+      }}
     >
       <span
         class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
