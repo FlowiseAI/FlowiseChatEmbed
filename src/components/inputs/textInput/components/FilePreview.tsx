506eef5ee19f05c52e4ddd67987718122cd30daa
Henry <hzj94@hotmail.com>
2024-08-23 02:05:26 +0100

    update linting

--- src/components/inputs/textInput/components/FilePreview.tsx
+++ src/components/inputs/textInput/components/FilePreview.tsx
@@ -11,24 +11,14 @@ export const FilePreview = (props: CardWithDeleteOverlayProps) => {
   const defaultBackgroundColor = 'transparent';
 
   return (
-    <div
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      class="relative inline-block"
-    >
+    <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} class="relative inline-block">
       <div
         class={`inline-flex items-center h-12 max-w-max p-2 mr-1 flex-none transition-opacity duration-300 ${
           isHovered() ? 'opacity-100' : 'opacity-100'
         } ${isHovered() ? 'bg-[rgba(0,0,0,0.3)]' : `bg-[${defaultBackgroundColor}]`} border border-gray-300 rounded-md`}
       >
-        <AttachmentIcon color='#303235' class={`transition-filter duration-300 ${isHovered() ? 'blur-[2px]' : 'blur-none'}`} />
-        <span
-          class={`ml-1.5 'text-inherit transition-filter duration-300 ${
-            isHovered() ? 'blur-[2px]' : 'blur-none'
-          }`}
-        >
-          {props.item.name}
-        </span>
+        <AttachmentIcon color="#303235" class={`transition-filter duration-300 ${isHovered() ? 'blur-[2px]' : 'blur-none'}`} />
+        <span class={`ml-1.5 'text-inherit transition-filter duration-300 ${isHovered() ? 'blur-[2px]' : 'blur-none'}`}>{props.item.name}</span>
       </div>
       {isHovered() && (
         <button
@@ -36,7 +26,7 @@ export const FilePreview = (props: CardWithDeleteOverlayProps) => {
           class="absolute top-0 left-0 right-0 bottom-0 bg-transparent hover:bg-transparent flex items-center justify-center"
           title="Remove attachment"
         >
-          <TrashIcon color='white' />
+          <TrashIcon color="white" />
         </button>
       )}
     </div>
