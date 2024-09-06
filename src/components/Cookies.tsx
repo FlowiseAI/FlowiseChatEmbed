088634d2373c66ea6e07a92ec62454d86296b292
KI-Experten <156334819+KI-Experten@users.noreply.github.com>
2024-09-06 22:11:40 +0200

    Create Cookies.tsx - Disclaimer Popup for GDPR-Compliance

---
+++ src/components/Cookies.tsx
@@ -0,0 +1,24 @@
+// Cookies.tsx
+
+export function setCookie(cname: string, cvalue: string, exdays: number) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+export function getCookie(cname: string): string {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
