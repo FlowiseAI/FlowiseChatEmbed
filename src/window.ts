4473d258b82738acf759880bd7641f497cd49925
Nurhamsah <nur.hamsah.cash@gmail.com>
2024-09-13 10:41:18 +0700

    change element name

--- src/window.ts
+++ src/window.ts
@@ -15,15 +15,15 @@ let elementUsed: Element | undefined;
 
 export const initFull = (props: BotProps & { id?: string }) => {
   destroy();
-  const fullElement = props.id ? document.getElementById(props.id) : document.querySelector('flowise-fullchatbot');
-  if (!fullElement) throw new Error('<flowise-fullchatbot> element not found.');
+  const fullElement = props.id ? document.getElementById(props.id) : document.querySelector('hegira-fullchatbot');
+  if (!fullElement) throw new Error('<hegira-fullchatbot> element not found.');
   Object.assign(fullElement, props);
   elementUsed = fullElement;
 };
 
 export const init = (props: BotProps) => {
   destroy();
-  const element = document.createElement('flowise-chatbot');
+  const element = document.createElement('hegira-chatbot');
   Object.assign(element, props);
   document.body.appendChild(element);
   elementUsed = element;
