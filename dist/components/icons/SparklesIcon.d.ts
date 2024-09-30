930f6a31109753a0fd856542261ce99dd317564f
Ilango <rajagopalilango@gmail.com>
2024-09-30 21:10:32 +0530

    Update follow-up prompts ui and styles

---
+++ dist/components/icons/SparklesIcon.d.ts
@@ -0,0 +1,3 @@
+import { JSX } from 'solid-js/jsx-runtime';
+export declare const SparklesIcon: (props: JSX.SvgSVGAttributes<SVGSVGElement>) => JSX.Element;
+//# sourceMappingURL=SparklesIcon.d.ts.map
