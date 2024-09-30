11b05123ca4eb7bc2ea26d7a445d6b067a0a54ef
Ilango <rajagopalilango@gmail.com>
2024-09-30 21:11:13 +0530

    Apply prettier

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1361,13 +1361,13 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
           <Show when={messages().length > 2 && followUpPromptsStatus()}>
             <Show when={followUpPrompts().length > 0}>
               <>
-                <div class='flex items-center gap-1 px-5'>
-                  <SparklesIcon class='w-4 h-4' />
+                <div class="flex items-center gap-1 px-5">
+                  <SparklesIcon class="w-4 h-4" />
                   <span class="text-sm text-gray-700">Try these prompts</span>
                 </div>
                 <div class="w-full flex flex-row flex-wrap px-5 py-[10px] gap-2">
                   <For each={[...followUpPrompts()]}>
-                  {(prompt, index) => (
+                    {(prompt, index) => (
                       <FollowUpPromptBubble
                         prompt={prompt}
                         onPromptClick={() => followUpPromptClick(prompt)}
