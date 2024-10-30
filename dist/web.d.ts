3ee1985fb6c0697560a6119651333ee9fd485ea3
Max Techera <maxi.techerag@gmail.com>
2024-10-30 12:25:05 -0300

    Update branding

--- dist/web.d.ts
+++ dist/web.d.ts
@@ -2,20 +2,16 @@ declare const chatbot: {
     initFull: (props: {
         chatflowid: string;
         apiHost?: string | undefined;
-        onRequest?: ((request: RequestInit) => Promise<void>) | undefined;
         chatflowConfig?: Record<string, unknown> | undefined;
         observersConfig?: import("./components/Bot").observersConfigType | undefined;
-        theme?: import("./features/bubble/types").BubbleTheme | undefined;
     } & {
         id?: string | undefined;
     }) => void;
     init: (props: {
         chatflowid: string;
         apiHost?: string | undefined;
-        onRequest?: ((request: RequestInit) => Promise<void>) | undefined;
         chatflowConfig?: Record<string, unknown> | undefined;
         observersConfig?: import("./components/Bot").observersConfigType | undefined;
-        theme?: import("./features/bubble/types").BubbleTheme | undefined;
     }) => void;
     destroy: () => void;
 };
