4af0a34a5f1494954845084a7aa976c0fbfdc245
digi-works <subhaarunpandian@Subhas-MacBook-Pro.local>
2025-08-30 13:07:00 +0530

    Updated branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,14 +51,14 @@ export const Badge = (props: Props) => {
           {props.footer?.text ?? 'Powered by'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? 'https://digiworks.ai'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Digiworks'}</span>
           </a>
         </span>
       </Show>
