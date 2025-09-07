2558c248de5de6a4c1f2a1585e6d6ffc643b07c5
tbwksm <ksm@tobeway.com>
2025-09-08 00:56:26 +0900

    refresh button, pdf icon

--- src/components/icons/ChevronDownIcon.tsx
+++ src/components/icons/ChevronDownIcon.tsx
@@ -6,18 +6,7 @@ type Props = {
 };
 
 export const ChevronDownIcon: Component<Props> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    class={props.class || 'w-4 h-4'}
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    stroke-width="2"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M19 9l-7 7-7-7"
-    />
+  <svg xmlns="http://www.w3.org/2000/svg" class={props.class || 'w-4 h-4'} fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
   </svg>
 );
